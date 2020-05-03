165767f36ba1e2678031a4af2a302a6ce7580ede
Harnet69 <harnet69@gmail.com>
2020-05-03 19:54:36 +0200

    Players can make a turn

--- app/src/main/java/com/example/tictaktoe/MainActivity.java
+++ app/src/main/java/com/example/tictaktoe/MainActivity.java
@@ -7,11 +7,19 @@ import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    private int activePlayer;
+
 
     public void dropIn(View view){
         ImageView counter = (ImageView) view;
         counter.setTranslationY(-1500);
-        counter.setImageResource(R.drawable.yellow);
+        if(activePlayer == 0) {
+            counter.setImageResource(R.drawable.yellow);
+            activePlayer = 1;
+        }else{
+            counter.setImageResource(R.drawable.red);
+            activePlayer = 0;
+        }
         counter.animate().translationYBy(1500);
     }
 
