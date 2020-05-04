701f86feac1765be507047851177eedffe02de48
Harnet69 <harnet69@gmail.com>
2020-05-04 14:32:53 +0200

    Velcoming text displays

--- app/src/main/java/com/example/tictaktoe/MainActivity.java
+++ app/src/main/java/com/example/tictaktoe/MainActivity.java
@@ -2,26 +2,23 @@ package com.example.tictaktoe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ImageView;
-
-import java.sql.SQLOutput;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private int[] gameState;
     int[][] winningPositions = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
-
+    private TextView gameStateDisplay;
     private static final int colsNum = 3;
     private static final int rowsNum = 3;
     private int activePlayer = 1;
 
-
+    @SuppressLint("SetTextI18n")
     public void dropIn(View view) {
         ImageView counter = (ImageView) view;
         if (gameState[(int) counter.getTag()] == 0) {
@@ -36,8 +33,16 @@ public class MainActivity extends AppCompatActivity {
             }
             counter.animate().translationYBy(1500);
 //            System.out.println(Arrays.toString(gameState));
-
-            }System.out.println(isWin());
+            if (isWinCombination()) {
+                gameStateDisplay.setText("Player WON!!!");
+                System.out.println("Win");
+            }
+            if(!isEmptyCellExist()){
+                gameStateDisplay.setText("It's a DRAW!!!");
+                System.out.println("It's a draw!");
+            }
+        } else {
+            System.out.println("Cell is occupied");
         }
     }
 
@@ -46,6 +51,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         addTagsToCells();
+        gameStateDisplay = findViewById(R.id.gameStateDisplay);
+
     }
 
     // initial adding tagg with cell number to ImageView
@@ -62,10 +69,27 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    public boolean winCondition(){
+        if(isWinCombination()){
+            return true;
+        }
+        return false;
+    }
+
     // win condition
-    public boolean isWin() {
+    public boolean isWinCombination() {
         for (int[] winningPosition : winningPositions) {
             if (gameState[winningPosition[0]] == gameState[winningPosition[1]] && gameState[winningPosition[1]] == gameState[winningPosition[2]] && gameState[winningPosition[0]] != 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // is empty cells exist
+    public boolean isEmptyCellExist() {
+        for (int value : gameState) {
+            if (value == 0) {
                 return true;
             }
         }
