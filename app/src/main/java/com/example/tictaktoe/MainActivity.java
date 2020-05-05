03fd9eacc630469a338f9cbd00303a90ba0dd8ca
Harnet69 <harnet69@gmail.com>
2020-05-05 11:14:29 +0200

    Restart partly works

--- app/src/main/java/com/example/tictaktoe/MainActivity.java
+++ app/src/main/java/com/example/tictaktoe/MainActivity.java
@@ -8,6 +8,7 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -15,57 +16,37 @@ import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
     private int[] gameState;
-    int[][] winningPositions = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
     private TextView gameStateDisplay;
+    private Button restartBtn;
+    int[][] winningPositions = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
     private int activePlayer = 1;
-
-    @SuppressLint("SetTextI18n")
-    public void dropIn(View view) {
-        ImageView counter = (ImageView) view;
-        if (gameState[(int) counter.getTag()] == 0) {
-            counter.setTranslationY(-1500);
-            gameState[(int) counter.getTag()] = activePlayer;
-            if (activePlayer == 1) {
-                counter.setImageResource(R.drawable.yellow);
-                activePlayer = 2;
-            } else {
-                counter.setImageResource(R.drawable.red);
-                activePlayer = 1;
-            }
-            counter.animate().translationYBy(1500);
-            if (isWinCombination()) {
-                if (activePlayer == 2) {
-                    gameStateDisplay.setText("Yellow Player WON");
-                } else {
-                    gameStateDisplay.setText("Red Player WON");
-                }
-                colorEmptyCells();
-            }
-            else if (!isEmptyCellExist()) {
-                gameStateDisplay.setText("It's a DRAW!!!");
-            }
-        } else {
-            counter.animate().rotationYBy(360).setDuration(1000);
-            System.out.println("Cell is occupied");
-        }
-        System.out.println(Arrays.toString(gameState));
-    }
+    private boolean refresh;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        if(refresh){
+            super.onCreate(null);
+        }else{
+            super.onCreate(savedInstanceState);
+        }
         setContentView(R.layout.activity_main);
-        addTagsToCells();
         gameStateDisplay = findViewById(R.id.gameStateDisplay);
+        restartBtn = findViewById(R.id.restartBtn);
+        addTagsToCells();
 
-        if (savedInstanceState != null) {
+        // get saved state
+        if (savedInstanceState != null && !refresh) {
             gameState = savedInstanceState.getIntArray("gameState");
             gameStateDisplay.setText(savedInstanceState.getString("gameStateDisplay"));
             addImageToImageView();
+            System.out.println("Refresh!!");
+        }else{
+//            super.onCreate(savedInstanceState);
+            refresh = false;
         }
     }
 
-    // initial adding tag with cell number to ImageView
+    // initial adding a tag with cell number to ImageView
     public void addTagsToCells() {
         ViewGroup yourLayout = (ViewGroup) findViewById(R.id.gridLayout);
         gameState = new int[yourLayout.getChildCount()];
@@ -77,6 +58,41 @@ public class MainActivity extends AppCompatActivity {
                 gameState[i] = 0;
             }
         }
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void dropIn(View view) {
+        ImageView counter = (ImageView) view;
+
+        if (gameState[(int) counter.getTag()] == 0) {
+            counter.setTranslationY(-1500);
+            gameState[(int) counter.getTag()] = activePlayer;
+            if (activePlayer == 1) {
+                counter.setImageResource(R.drawable.yellow);
+                activePlayer = 2;
+            } else {
+                counter.setImageResource(R.drawable.red);
+                activePlayer = 1;
+            }
+            counter.animate().translationYBy(1500);
+
+            if (isWinCombination()) {
+                if (activePlayer == 2) {
+                    gameStateDisplay.setText("Yellow Player WON");
+                } else {
+                    gameStateDisplay.setText("Red Player WON");
+                }
+                colorEmptyCells();
+            }
+            else if (!isEmptyCellExist()) {
+                gameStateDisplay.setText("It's a DRAW!!!");
+            }
+
+        } else {
+            counter.animate().rotationYBy(360).setDuration(1000);
+            System.out.println("Cell is occupied");
+        }
+//        System.out.println(Arrays.toString(gameState));
     }
 
     // add images to ImageView after changing orientation
@@ -109,7 +125,7 @@ public class MainActivity extends AppCompatActivity {
     // is empty cells exist
     public boolean isEmptyCellExist() {
         for (int value : gameState) {
-            if (value == 0 || value == 404) {
+            if (value == 0) {
                 return true;
             }
         }
@@ -151,6 +167,22 @@ public class MainActivity extends AppCompatActivity {
         super.onSaveInstanceState(outState);
         outState.putIntArray("gameState", gameState);
         outState.putString("gameStateDisplay", (String) gameStateDisplay.getText());
+    }
+
+
+
+    public void restart(View view){
+        System.out.println("Restart!");
+        for(int i = 0; i < gameState.length; i++){
+            gameState[i] = 0;
+        }
+        refresh = true;
+        System.out.println(Arrays.toString(gameState));
+//        ViewGroup vg = findViewById (R.id.allView);
+//        vg.invalidate();
+//        public void onCreate(Bundle savedInstanceState){
+//            super.onCreate(null);
+//        }
     }
 
 //    @Override
